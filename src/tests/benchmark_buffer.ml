2adc43e22e576653da033b40fecbf7e168cf3790
David Mallows <dave.mallows@gmail.com>
2018-09-13 10:55:30 +0100

    Fastest possible approach: monomorphic, all perf tricks, no bounds checking

--- src/tests/benchmark_buffer.ml
+++ src/tests/benchmark_buffer.ml
@@ -4,41 +4,28 @@ type t = (char, int8_unsigned_elt, c_layout) Array1.t
 external set_int64 : t -> int -> int64 -> unit = "%caml_bigstring_set64u" [@@noalloc]
 external get_int64 : t -> int -> int64 = "%caml_bigstring_get64u"
 
-type 'a iface = {
-  get_int64 : t -> int -> int64;
-  set_int64 : t -> int -> int64 -> unit;
-}
-
-type 'a store = {
-  iface : 'a iface;
-  value : 'a
-}
-
-let bigstring_iface = {get_int64; set_int64}
-
-type box = Box of t
-
 let buffer_write_integers () =
   let m = 100000000 in
-  let bb = {value = (Array1.create char c_layout (8 * m)); iface=bigstring_iface} in
-  let rec f : int -> t iface -> t -> unit = fun n iface value ->
+  let b = Array1.create char c_layout (m * 8) in
+  (* Array1.fill b '\x00'; *)
+  let rec f : int -> t -> unit = fun n b ->
     match n with
     | n when n = m -> ()
     | n ->
-      iface.set_int64 value (n * 8) (Int64.of_int n);
-      f (n + 1) iface value
+      set_int64 b (n * 8) (Int64.of_int n);
+      f (n + 1) b
   in
-  f 0 bb.iface bb.value;
+  f 0 b;
 
-  let rec g : int -> t iface -> t ->  unit = fun n iface value ->
+  let rec g : int -> t -> unit = fun n b ->
     match n with
     | n when n = m -> ()
     | n ->
-      let v = iface.get_int64 value (n * 8) in
+      let v = get_int64 b (n * 8) in
       if v <> (Int64.of_int n) then failwith "Mismatch";
-      g (n+1) iface value
+      g (n+1) b
   in
-  g 0 bb.iface bb.value
+  g 0 b
 
 
 let () = buffer_write_integers ()
