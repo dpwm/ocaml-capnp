91cb2b6fea30621e2545762fcec2ec427e334a01
David Mallows <dave.mallows@gmail.com>
2018-09-13 11:27:23 +0100

    With polymorphism through first-class modules. 2x speed-down.

--- src/tests/benchmark_buffer.ml
+++ src/tests/benchmark_buffer.ml
@@ -1,31 +1,72 @@
 open Bigarray
+(* This takes
 
-type t = (char, int8_unsigned_elt, c_layout) Array1.t
-external set_int64 : t -> int -> int64 -> unit = "%caml_bigstring_set64u" [@@noalloc]
-external get_int64 : t -> int -> int64 = "%caml_bigstring_get64u"
+real	0m2.003s
+user	0m1.719s
+sys	0m0.276s
 
-let buffer_write_integers () =
+*)
+
+module BigString = struct
+  type t = (char, int8_unsigned_elt, c_layout) Array1.t
+  let alloc m = Array1.create char c_layout m
+  external set_int64 : t -> int -> int64 -> unit = "%caml_bigstring_set64u" [@@noalloc]
+  external get_int64 : t -> int -> int64 = "%caml_bigstring_get64u"
+end
+
+module Bytes = struct
+  type t = Bytes.t
+  let alloc m = Bytes.create m
+  external set_int64 : t -> int -> int64 -> unit = "%caml_bytes_set64u" [@@noalloc]
+  external get_int64 : t -> int -> int64 = "%caml_bytes_get64u"
+end
+
+type t =
+    | Bytes of Bytes.t
+    | Bigstring of BigString.t
+
+let bytes_alloc n = Bytes (Bytes.alloc n)
+let bigstring_alloc n = Bigstring (BigString.alloc n)
+
+let set_int64 b n v =
+  match b with
+  | Bytes b -> Bytes.set_int64 b n v
+  | Bigstring b -> BigString.set_int64 b n v
+
+let get_int64 b n =
+  match b with
+  | Bytes b -> Bytes.get_int64 b n
+  | Bigstring b -> BigString.get_int64 b n
+
+module type CONTAINED = sig
+  type t
+  val alloc : int -> t
+  val set_int64 : t -> int -> int64 -> unit
+  val get_int64 : t -> int -> int64
+end
+
+
+let buffer_write_integers (module X : CONTAINED) =
   let m = 100000000 in
-  let b = Array1.create char c_layout (m * 8) in
-  (* Array1.fill b '\x00'; *)
-  let rec f : int -> t -> unit = fun n b ->
+  let b = X.alloc (m * 8) in
+  let rec f : int -> X.t -> unit = fun n b ->
     match n with
     | n when n = m -> ()
     | n ->
-      set_int64 b (n * 8) (Int64.of_int n);
+      X.set_int64 b (n * 8) (Int64.of_int n);
       f (n + 1) b
   in
   f 0 b;
 
-  let rec g : int -> t -> unit = fun n b ->
+  let rec g : int -> X.t -> unit = fun n b ->
     match n with
     | n when n = m -> ()
     | n ->
-      let v = get_int64 b (n * 8) in
+      let v = X.get_int64 b (n * 8) in
       if v <> (Int64.of_int n) then failwith "Mismatch";
       g (n+1) b
   in
   g 0 b
 
 
-let () = buffer_write_integers ()
+let () = buffer_write_integers (module BigString : CONTAINED)
